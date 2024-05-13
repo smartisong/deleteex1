b0a727b7b5e5e13eaa27b10cf7c3fd790fb8fea3
DESKTOP-048HU3O\ECS <wodlr1310@naver.com>
2024-05-13 12:35:31 +0900

    커밋 5
    주석처

--- hello.java
+++ hello.java
@@ -1,7 +1,10 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
+     
+        //주석처리
         int a = 10;
+        //주석처리2
         int b = 10;
         System.out.println(a + b);
     }
