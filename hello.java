823535b6da2006f0615bfb5e7dc25cbb61d7edfd
DESKTOP-048HU3O\ECS <elfflqjem3366@naver.com>
2024-05-13 12:35:29 +0900

    개빡치니까 퇴사합니다

--- hello.java
+++ hello.java
@@ -1,8 +1,5 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        int a = 10;
-        int b = 10;
-        System.out.println(a + b);
+      
     }
 }
